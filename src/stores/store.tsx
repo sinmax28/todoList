bde494ac0167c8f85f4b5c08e562c515a52574a4
sinmax28 <cosmin.stretea@rebeldot.com>
2021-07-26 19:34:36 +0300

    installed toolkit package

--- src/stores/store.tsx
+++ src/stores/store.tsx
@@ -1,8 +1,18 @@
 import {createStore, applyMiddleware} from 'redux';
 import reduxThunk from 'redux-thunk';
-import {persistStore, persistReducer} from 'redux-persist';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import rootReducer from './rootReducer';
+import {configureStore} from '@reduxjs/toolkit';
 
 const persistConfig = {
   key: 'root',
@@ -10,8 +20,17 @@ const persistConfig = {
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
-const store = createStore(persistedReducer, applyMiddleware(reduxThunk));
+// const store = createStore(persistedReducer, applyMiddleware(reduxThunk));
 
+export const store = configureStore({
+  reducer: persistReducer,
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
 export const persistor = persistStore(store);
 export default store;
 export type RootState = ReturnType<typeof store.getState>;
